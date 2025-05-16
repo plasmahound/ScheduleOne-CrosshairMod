9bf12ed7cf40e5ac8d7b3f14c951480475ac1d30
plasmahound <59979397+plasmahound@users.noreply.github.com>
2025-05-15 20:29:49 -0700

    Add project files.

---
+++ EZCrosshair/Core.cs
@@ -0,0 +1,15 @@
+﻿using MelonLoader;
+
+[assembly: MelonInfo(typeof(EZCrosshair.Core), "EZCrosshair", "1.0.0", "plasmahound", null)]
+[assembly: MelonGame("TVGS", "Schedule I")]
+
+namespace EZCrosshair
+{
+	public class Core : MelonMod
+	{
+		public override void OnInitializeMelon()
+		{
+			LoggerInstance.Msg("Initialized.");
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# EZ Crosshair Mod
+A mod for **Schedule I** that adds a crosshair to the game.
+
+---
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
